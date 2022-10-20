94d0be864f6af38a1a62ca2ab3f58037ee7d2d8f
Kamil Wozniak <kamil.wozniak@plextek.com>
2022-10-20 10:19:44 +0100

    Adds bug report app

---
+++ UpdaterThrottlerCrasher/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System.Windows;
+
+namespace UpdaterThrottlerCrasher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Configure LiveCharts2
+            LiveCharts.Configure(config =>
+                config
+                    // registers SkiaSharp as the library back end
+                    // REQUIRED unless you build your own
+                    .AddSkiaSharp()
+
+                    // adds the default supported types
+                    // OPTIONAL but highly recommend
+                    .AddDefaultMappers()
+
+                    // select a theme, default is Light
+                    // OPTIONAL
+                    //.AddDarkTheme()
+                    .AddLightTheme()
+            );
+        }
+    }
+}
